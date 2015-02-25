b630b24a5cc00a1e79ce3e11f4c8553394093838
Aleksandr Sobol <bonpet666@gmail.com>
2015-02-25 14:43:29 +0300

    added linear search

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+
 
 /**
  * Created by admin on 25.02.2015.
@@ -14,7 +14,23 @@ public class BinarySearch {
         int required = 33; //искомое
         int lowerBound = 0; //нижняя половина
         int upperBound = intArray.length - 1; //верхняя половина
-        int half;
+        int half; //половина ряда
+        int q = 1; //счетчик операций
+
+        for (int j=0; j<intArray.length; j++){
+            if (intArray[j] == required){
+                System.out.println("Количество операций при линейном поиске: " + (j+1));
+                break;
+            }
+        }
+        if (intArray.length == required){
+            System.out.println("Элемент не найден!");
+        } else {
+            System.out.println("Элемент найден: " + required);
+        }
+
+
+
         while (true){
             half = (lowerBound + upperBound)/2;
             if(intArray[half]==required){
@@ -29,8 +45,10 @@ public class BinarySearch {
                 } else {
                     upperBound = half - 1;
                 }
+                q++;
             }
         }
+        System.out.println("Количество операций при бинарном поиске:" + q);
     }
 
 }
